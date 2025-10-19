6c65dad42de64a8719f47f50671c6bf42f22198c
ansman58 <javanslem@gmail.com>
2025-10-19 15:34:38 +0100

    update

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -3,6 +3,7 @@ import { adminGuard } from "@/lib/admin";
 import { fetchAllUsers } from "@/services/admin.service";
 import React from "react";
 import UsersList from "@/components/admin/UsersList";
+import { IUserRole } from "@/interface/user";
 
 type ISearchParams = Promise<{
   search?: string;
@@ -22,7 +23,7 @@ const Page = async ({ searchParams }: { searchParams: ISearchParams }) => {
     search: queries.search,
     page: queries.page ? parseInt(queries.page) : 1,
     per_page: queries.per_page ? parseInt(queries.per_page) : 50,
-    role,
+    role: role as IUserRole,
   });
   
   if (!users?.success) {
